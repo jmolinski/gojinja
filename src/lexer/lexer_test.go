c1ca55c51106a94ec9dc0c059109804c99d4962d
Hubert Badocha <badocha.hubert@gmail.com>
2022-07-21 21:03:05 +0200

    Simple test

--- src/lexer/lexer_test.go
+++ src/lexer/lexer_test.go
@@ -1,7 +1,22 @@
 package lexer
 
-import "testing"
+import (
+	"testing"
+)
+
+const example = `
+{% if name != "OFF" %}
+my name is {{ name }}
+{% endif %}
+{{ 5 + 1 }}
+`
 
 func Test(t *testing.T) {
-	// Just to ensure module compilation.
+	info := &EnvLexerInformation{}
+	l := GetLexer(info)
+	s, err := l.Tokenize(example, nil, nil, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(s)
 }
