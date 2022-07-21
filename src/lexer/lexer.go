b8f8434867e6fe8dac8e65250a66d47ae0680c2b
Hubert Badocha <badocha.hubert@gmail.com>
2022-07-21 20:42:26 +0200

    Post rebase fix

--- src/lexer/lexer.go
+++ src/lexer/lexer.go
@@ -67,21 +67,6 @@ type rule struct {
 	command *string
 }
 
-type EnvLexerInformation struct {
-	BlockStartString    string
-	BlockEndString      string
-	VariableStartString string
-	VariableEndString   string
-	CommentStartString  string
-	CommentEndString    string
-	LineStatementPrefix *string
-	LineCommentPrefix   *string
-	TrimBlocks          bool
-	LStripBlocks        bool
-	NewlineSequence     string
-	KeepTrailingNewline bool
-}
-
 type Lexer struct {
 	LStripBlocks        bool
 	NewlineSequence     string
